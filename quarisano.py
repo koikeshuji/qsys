7ca33642458f78f6a1368b8996124294ce4000ea
Nao Yonashiro @orisano <orisano.owan@gmail.com>
2016-12-06 03:51:27 +0900

    implement naive payload aggregates

--- quarisano.py
+++ quarisano.py
@@ -1,3 +1,4 @@
+import editdistance
 from sklearn.covariance import EmpiricalCovariance
 
 from collections import Counter, defaultdict, namedtuple
@@ -8,6 +9,8 @@ import pickle
 DEFAULT_RELIABILITY = 0.5
 DEFAULT_BLOCK = 0.3
 DEFAULT_THRESH = 4.0
+DEFAULT_EDIT_THRESH = 64
+DEFAULT_CLUSTER_PER = 0.2
 ETA = 0.03
 
 
@@ -37,6 +40,11 @@ def _clamp(x, mini, maxi):
     return max(min(x, maxi), mini)
 
 
+def freq_byte(payload):
+    freq = Counter(payload)
+    return [freq[i] for i in range(256)]
+
+
 QPacket = namedtuple("QPacket", "src_ip dst_ip payload")
 
 
@@ -55,6 +63,7 @@ class Quarisano(object):
         self.subnets = [_parse_subnet(subnet) for subnet in subnets]
         self.reliability = defaultdict(lambda: DEFAULT_RELIABILITY)
         self.packet_log = defaultdict(list)
+        self.payload_log = defaultdict(list)
         self.known_ip = set()
         self.thresh = thresh
         self.block = block
@@ -73,7 +82,11 @@ class Quarisano(object):
         self._update_log(packet)
         src_ip = _parse_ip(packet.src_ip)
         rel = self._update_reliability(src_ip)
-        print(packet)
+        
+        if len(self.known_ip) >= 2 and packet.payload:
+            cmd_prob = self._get_command_prob(packet)
+            self.reliability[src_ip] += (0.3 - cmd_prob) * ETA
+
         print("rel:", rel)
         return rel > self.block
 
@@ -103,6 +116,28 @@ class Quarisano(object):
         mdist = EmpiricalCovariance().fit(mat).mahalanobis([vec])[0]
         return mdist
 
+    def _get_command_prob(packet):
+        src_ip = _parse_ip(packet.src_ip)
+        eds = {
+            ip: [editdistance.eval(packet.payload, x) for x in self.payload_log[ip]]
+            for ip in self.known_ip
+        }
+        near = {
+            ip: [x for x in eds[ip] if x < DEFAULT_EDIT_THRESH]
+            for ip in self.known_ip
+        }
+        per = {
+            ip: len(near[ip]) / len(eds[ip])
+            for ip in self.known_ip
+        }
+        my = per[src_ip]
+        other_avg = sum(per[ip] for ip in self.known_ip if ip != src_ip) / (len(self.known_ip) - 1)
+        
+        if my > other_avg:
+            return (my - other_avg) * (1 - other_avg)
+        else:
+            return 0.0
+
     def _build_matrix(self):
         return [
             self._build_vector(ip)
@@ -117,6 +152,8 @@ class Quarisano(object):
     def _update_log(self, packet):
         src_ip = _parse_ip(packet.src_ip)
         self.packet_log[src_ip].append(self._get_subnet_id(packet.dst_ip))
+        if packet.payload:
+            self.payload_log[src_ip].append(packet.payload)
         self.known_ip.add(src_ip)
 
     def _get_subnet_id(self, ip):
